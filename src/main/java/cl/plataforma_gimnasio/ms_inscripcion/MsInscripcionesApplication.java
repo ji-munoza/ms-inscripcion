3c83b6069e9d72741c16e5c34ef4864e9b934a59
ji-munoza <ji.munoza@duocuc.cl>
2026-05-17 16:59:29 -0400

    corregido nombre package

--- src/main/java/cl/plataforma_gimnasio/ms_inscripcion/MsInscripcionesApplication.java
+++ src/main/java/cl/plataforma_gimnasio/ms_inscripcion/MsInscripcionesApplication.java
@@ -1,4 +1,4 @@
-package cl.plataforma_gimnasio.ms_inscripciones;
+package cl.plataforma_gimnasio.ms_inscripcion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/cl/plataforma_gimnasio/ms_inscripcion/MsInscripcionesApplicationTests.java
+++ src/test/java/cl/plataforma_gimnasio/ms_inscripcion/MsInscripcionesApplicationTests.java
@@ -1,4 +1,4 @@
-package cl.plataforma_gimnasio.ms_inscripciones;
+package cl.plataforma_gimnasio.ms_inscripcion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
